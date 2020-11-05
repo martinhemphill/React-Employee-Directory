1ab53740f64bd5bf07967008839fc5c480547ea6
martinhemphill <mthemphi@gmail.com>
2020-11-05 18:58:45 -0500

    App, Index, Navbar and Mainpage files, main page not filled

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <a className="nav-link active" href="/">
+        Employee Directory
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
